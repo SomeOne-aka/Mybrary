0a1eb1152fa5f1f7730f2e04d9e78a2d56f24da5
SomeOne-aka <tred63957cs@gmail.com>
2023-11-07 12:16:28 +0100

    delete  multer and use FilePond and store files in the MongoDB

--- routes/books.js
+++ routes/books.js
@@ -2,17 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Book = require("./../models/book");
 const Author = require("./../models/author");
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
-const uploadPath = path.join("public", Book.coverImageBasePath);
-const imageMemeType = ["images/jpg", "images/jpeg", "images/png", "images/gif"];
-const upload = multer({
-  dest: uploadPath,
-  fileFilter: (req, file, cb) => {
-    cb(null, imageMemeType);
-  },
-});
+const imageMemeTypes = ["image/jpg", "image/jpeg", "image/png", "image/gif"];
 
 // All book routes
 router.get("/", async (req, res) => {
@@ -46,23 +36,21 @@ router.get("/new", async (req, res) => {
 });
 
 // Create books route
-router.post("/", upload.single("cover"), async (req, res) => {
-  const fileName = req.file != null ? req.file.filename : null;
+router.post("/", async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    coverImageName: fileName,
     description: req.body.description,
   });
+
+  saveCover(book, req.body.cover);
+
   try {
     const newBook = await book.save();
     res.redirect("/books");
   } catch (error) {
-    if (book.coverImageName) {
-      removeBookCover(book.coverImageName);
-    }
     renderNewPage(res, book, true);
   }
 });
@@ -81,10 +69,13 @@ async function renderNewPage(res, book, hasError = false) {
   }
 }
 
-function removeBookCover(fileName) {
-  fs.unlink(path.join(uploadPath, fileName), (err) => {
-    if (err) console.error(err);
-  });
+function saveCover(book, coverEncoded) {
+  if (coverEncoded == null) return;
+  const cover = JSON.parse(coverEncoded);
+  if (cover != null && imageMemeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from(cover.data, "base64");
+    book.coverImageType = cover.type;
+  }
 }
 
 module.exports = router;
