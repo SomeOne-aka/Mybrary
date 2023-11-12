39333ab27116f2b22c28619f8e5032e17fb59525
SomeOne-aka <tred63957cs@gmail.com>
2023-11-12 13:33:53 +0100

    finish project

--- public/javascript/fileUpload.js
+++ public/javascript/fileUpload.js
@@ -1,13 +1,29 @@
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginFileEncode
-);
+const rootStyles = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-});
+if (rootStyles.getPropertyValue("--book-cover-width-large") != null) {
+  ready();
+} else {
+  document.getElementById("main-css").addEventListener("load", ready);
+}
 
-FilePond.parse(document.body);
+function ready() {
+  const coverWidth = rootStyles.getPropertyValue("--book-cover-width-large");
+  const coverAspectRatio = rootStyles.getPropertyValue(
+    "--book-cover-aspect-ratio"
+  );
+  const coverHeight = eval(coverWidth / coverAspectRatio);
+
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode
+  );
+
+  FilePond.setOptions({
+    stylePanelAspectRatio: parseFloat(coverAspectRatio),
+    imageResizeTargetWidth: parseFloat(coverWidth),
+    imageResizeTargetHeight: parseFloat(coverHeight),
+  });
+
+  FilePond.parse(document.body);
+}
